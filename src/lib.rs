f66788e737bf95b36eb58cd62cef2a46b25071ca
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-06-01 13:14:20 -0700

    initial struct, test

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,20 @@
-// unimplemented!()
+pub struct Archeon {
+    pub ignited: bool,
+}
+
+impl Archeon {
+    pub async fn ignite() -> Archeon {
+        Archeon { ignited: true }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn ignite() {
+        let test_archeon = Archeon::ignite().await;
+        assert_eq!(test_archeon.ignited, true);
+    }
+}
