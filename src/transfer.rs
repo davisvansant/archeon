30c47a9acdfc91fa1e92bae31d25684f6cec5b5d
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-06-04 13:48:43 -0700

    initial impl of get_content_length, tests

--- src/transfer.rs
+++ src/transfer.rs
@@ -1,5 +1,6 @@
 use hyper::client::connect::HttpConnector;
-use hyper::{Body, Client, Uri};
+use hyper::header::{HeaderValue, CONTENT_LENGTH};
+use hyper::{Body, Client, Request, Uri};
 use hyper_tls::HttpsConnector;
 
 use std::str::FromStr;
@@ -22,11 +23,30 @@ impl Transfer {
             initialized: true,
         }
     }
+
+    async fn get_content_length(&self) -> HeaderValue {
+        let request = Request::head(&self.uri)
+            .body(Body::empty())
+            .expect("Could not Build Request!");
+
+        let response = self.client.request(request).await;
+        let response_parts = match response {
+            Ok(response) => response.into_parts(),
+            Err(error) => panic!("{}", error),
+        };
+        let content_length = response_parts.0.headers.get(CONTENT_LENGTH);
+        if let Some(header_value) = content_length {
+            header_value.to_owned()
+        } else {
+            panic!("Could not retrieve 'Content-Length' header!")
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use mockito::mock;
 
     #[tokio::test(flavor = "multi_thread")]
     async fn transfer() {
@@ -43,5 +63,21 @@ mod tests {
             "/with/path/and/query",
         );
         assert_eq!(test_transfer.initialized, true);
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn get_content_length() -> Result<(), hyper::Error> {
+        let test_mock_url = mockito::server_url();
+        let test_transfer = Transfer::init(&test_mock_url).await;
+        let mock = mock("HEAD", "/")
+            .with_status(200)
+            .with_header("Content-Length", "100000")
+            .with_body("")
+            .create();
+        let test_content_length_value = test_transfer.get_content_length().await;
+        mock.assert();
+        assert!(mock.matched());
+        assert_eq!(test_content_length_value.to_str().unwrap(), "100000");
+        Ok(())
     }
 }
